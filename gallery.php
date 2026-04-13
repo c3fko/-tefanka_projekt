6702d6290c2121c30b31ed9b407755283e14bf90
c3fko <richard.stefanka347@gmail.com>
2026-04-13 19:00:35 +0200

    footer
    to isté čo header ale miesto cesty do "header.php" je cesta do "footer.php"

--- gallery.php
+++ gallery.php
@@ -60,31 +60,12 @@
   </section>
 
   <!-- Footer -->
-  <footer class="bg-dark text-white py-4 mt-auto">
-    <div class="container">
-      <div class="row g-4">
-        <div class="col-md-4">
-          <h5>O stránke</h5>
-          <p>Stručné privítanie do sveta parkouru</p>
-        </div>
-        <div class="col-md-4">
-          <h5>Kontakt</h5>
-          <p>Email: <a href="mailto:parkour@mojastranka.sk" class="text-white">info@mojastranka.sk</a></p>
-          <p>Tel: <a href="tel:+421900123456" class="text-white">+421 900 123 456</a></p>
-        </div>
-        <div class="col-md-4">
-          <h5>Rýchle odkazy</h5>
-          <ul class="list-unstyled mb-0">
-            <li><a href="gallery.php" class="text-white">Galéria</a></li>
-            <li><a href="contacts.php" class="text-white">Kontakt</a></li>
-          </ul>
-        </div>
-      </div>
-      <div class="text-center mt-3">
-        <p class="mb-0">&copy; 2025 Richard Štefanka - Moja stránka</p>
-      </div>
-    </div>
-  </footer>
+   <?php
+  $file_path = "parts/footer.php"; 
+  if(!include($file_path)) {
+    echo"Failed to include $file_path";
+  } 
+  ?>
 
   <!-- kreatívny bod -->
   <!-- Cookie popup -->
