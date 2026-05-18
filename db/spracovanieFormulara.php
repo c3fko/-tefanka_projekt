24b5a40e2221cb26170f01f3d4561886e1ef451f
c3fko <richard.stefanka347@gmail.com>
2026-05-18 20:50:40 +0200

    OOP - db
    postovanie formulara zmenene na oop

--- db/spracovanieFormulara.php
+++ db/spracovanieFormulara.php
@@ -1,46 +1,24 @@
 <?php
 
-// PDO databázové pripojenie
-$host = "localhost";
-$dbname = "formular";
-$port = 3306;
-$username = "root";
-$password = "";
+require_once('../classes/kontakt.php');
+use formular\Kontakt;
 
-// Možnosti
-$options = array(
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-);
+$meno = $_POST['meno'];
+$email = $_POST['email'];
+$sprava = $_POST['sprava'];
 
-// Pripojenie PDO
-try {
-    $conn = new PDO("mysql:host=".$host.";dbname=".$dbname.";port=".$port, $username, $password, $options);
-} catch (PDOException $e) {
-    die("Chyba pripojenia: " . $e->getMessage());
-}
-?>
-
-<?php
-
-// Získanie údajov z formulára
-$meno = $_POST["meno"];
-$email = $_POST["email"];
-$sprava = $_POST["sprava"];
-
-// SQL príkaz INSERT
-$sql = "INSERT INTO udaje (meno, email, sprava)
-        VALUE ('".$meno."', '".$email."', '".$sprava."')";
-
-$statement = $conn->prepare($sql);
-
-try {
-    $insert = $statement->execute();
-    header("Location: http://localhost/štefanka_projekt/thankyoupage.php");
-    return $insert;
-} catch (Exception $exception) {
-    return false;
+// Overenie údajov
+if (empty($meno) || empty($email) || empty($sprava)) {
+    die('Chyba: Všetky polia sú povinné!');
 }
 
-// Zatvorenie pripojenia
-$conn = null;
+// Uloženie správy do databázy
+$kontakt = new Kontakt();
+$ulozene = $kontakt->ulozitSpravu($meno, $email, $sprava);
+
+if ($ulozene) {
+    header('Location: http://localhost/štefanka_projekt/thankyoupage.php');
+} else {
+    die('Chyba pri odosielaní správy do databázy!');
+    http_response_code(404);
+}
